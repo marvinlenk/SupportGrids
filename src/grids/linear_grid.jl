2929aa0fbc10010aa9eef1faefc91fef52862c28
Marvin Lenk <mlenk1990@gmail.com>
2024-02-16 18:22:50 +0100

    Polished LinearGrid documentation and removed @unpack macros.

--- src/grids/linear_grid.jl
+++ src/grids/linear_grid.jl
@@ -1,72 +1,103 @@
 """
-Data structure for operations in data defined on a linear Grid
+    LinearGrid(xmin::Real, xmax::Real, len::Int; complex_op=false, T::Type=Float64)
+
+Generates an equidistant grid from `xmin` to `xmax` with `len` points. If operations on
+complex arrays are expected, `complex_op` should be set to `true`.
+
+# Fields
+- `xmin`, `xmax`, `len` defined above.
+- `points`: Vector of grid points `pᵢ`.
+- `δ`: Discretisation parameter of `points`, i.e. `pᵢ₊₁ = pᵢ + δ`.
+- `op`: Tools for operations on this `LinearGrid`.
+
+See also [`LinearGridOps`](@ref).
 """
-struct LinearGridOps{T<:Real} <: AbstractGridOps{T}
-  weights::Vector{T}                      # weights for integration
-  FT::Plan                                # Fourier transform
-  FT⁻¹::ScaledPlan                        # inv Fourier transform
-  padded::Vector{<:Union{T, Complex{T}}}  # padded vector
-  f1::Vector{Complex{T}}                  # output of Fourier transform
-  f2::Vector{Complex{T}}                  # output of Fourier transform
-  out::Vector{<:Union{T, Complex{T}}}     # output of inv Fourier transform
-  conv_shift::Vector{Complex{T}}          # frequency shift for convolutions
-  corr_shift::Vector{Complex{T}}          # frequency shift for correlations
-  fft_1_x::Vector{Complex{T}}             # Fourier transform of 1/x
+struct LinearGrid{T} <: SupportGrid{T}
+  xmin::T               # Left bound
+  xmax::T               # Right bound
+  len::Int              # Number of points
+  points::Vector{T}     # Array of grid points
+  δ::T                  # Frequency discretisation
+  op::LinearGridOps{T}
 
-  function LinearGridOps(points::Vector{T}, complex_op::Bool; integration_method::Symbol=:trapz) where {T<:Real}
-    weights = integration_weights(points, integration_method)
-    L2 = nextpow(2, 2*length(points)-1) # size of padded operations
-    if T <: BigFloat L2 -= 1 end
-
-    padded = zeros(complex_op ? Complex{T} : T, L2)
-    out = similar(padded)
-    FT = complex_op ? plan_fft(out) : plan_rfft(out)
-    f1 = zeros(Complex{T}, FT.osz)
-    f2 = zeros(Complex{T}, FT.osz)
-    FT⁻¹ = complex_op ? plan_ifft(f1) : plan_irfft(f1, L2)
-
-    freqs = complex_op ? fftfreq(L2) : rfftfreq(L2)
-    dx = points[2] - points[1]
-    kₐ = points[1] * 2pi * freqs / dx
-
-    # NOTE: already normalized (dx and scale)
-    conv_shift = exp.(-1im * kₐ) * dx * FT⁻¹.scale
-    corr_shift = conj.(conv_shift)
-    fft_1_x = -1im * sign.(freqs) * FT⁻¹.scale
-
-    new{T}(weights,FT,FT⁻¹,padded,f1,f2,out,conv_shift,corr_shift,fft_1_x)
+  function LinearGrid(xmin::Real, xmax::Real, len::Int;
+      complex_op::Bool=false, T::Type=Float64
+    )
+    δ = (xmax - xmin) / (L - 1)
+    points = collect(range(xmin, stop=xmax, length=L))
+    new{T}(xmin, xmax, L, dx, points, LinearGridOps(points, complex_op; δ))
   end
+  #
+  LinearGrid(::Type{T}, args...; kwargs...) where T = LinearGrid(args...; T, kwargs...)
 end
 
 """
-Linear grid
-"""
-struct LinearGrid{T<:Real} <: SupportGrid{T}
-  xmin::T              # Left bound
-  xmax::T              # Right bound
-  L::Int               # Number of points
-  dx::T                # Frequency discretisation
-  points::Vector{T}
-  op::LinearGridOps{T}
+    LinearGridOps(points::Vector{T}, complex_op::Bool;
+                  integration_method::Symbol=:trapz,
+                  δ::T=-(extrema(points)...)/(length(points)-1)) where T
 
-  function LinearGrid(xmin::Real, xmax::Real, L::Int; complex_op=false,
-      T::Type{<:Real}=Float64)
-    dx = (xmax - xmin) / (L - 1)
-    points = collect(range(xmin, stop=xmax, length=L))
-    new{T}(xmin, xmax, L, dx, points, LinearGridOps(points, complex_op))
+Generates the necessary tools for operations on the `LinearGrid` with `points` of
+discretization `δ`. For `integration_method` options, see [`integral_weights`](@ref).
+
+# Fields
+- `weights`: Integration weights.
+- `ft`, `ft⁻¹`: FFT and inverse FFT plans by [`FFTW`](@ref).
+- `frequencies`: Points in Fourier space.
+- `padded`: Work array for padded vector / FFT input.
+- `ft_work1`, `ft_work2`, `ft⁻¹_work`: Work array for (inverse) FFT output.
+- `conv_shift`, `xcorr_shift`: FFT Phase correction arrays due to frequency shift.
+- `fft_x⁻¹`: Fourier transform of `1/x` used for the Hilbert transform.
+
+See also [`LinearGridOps`](@ref).
+"""
+struct LinearGridOps{T} <: AbstractGridOps{T}
+  weights::Vector{T}
+  ft::Plan
+  ft⁻¹::ScaledPlan
+  frequencies::Vector{Complex{T}}
+  padded::Vector{<:Union{T, Complex{T}}}
+  ft_work1::Vector{Complex{T}}
+  ft_work2::Vector{Complex{T}}
+  ft⁻¹_work::Vector{<:Union{T, Complex{T}}}
+  conv_shift::Vector{Complex{T}}
+  xcorr_shift::Vector{Complex{T}}
+  fft_x⁻¹::Vector{Complex{T}}
+  
+  function LinearGridOps(points::Vector{T}, complex_op::Bool;
+      integration_method::Symbol=:trapz, δ::T=-(extrema(points)...)/(length(points)-1)
+      ) where T
+    weights = integration_weights(points, integration_method)
+    len_padded = nextpow(2, 2*length(points)-1) # make it a power of 2
+    if T <: BigFloat len_padded -= 1 end # Why?
+    
+    padded = zeros(complex_op ? Complex{T} : T, len_padded)
+    ft⁻¹_work = similar(padded)
+    ft = complex_op ? plan_fft(ft⁻¹_work) : plan_rfft(ft⁻¹_work)
+    ft_work1 = zeros(Complex{T}, ft.osz)
+    ft_work2 = zeros(Complex{T}, ft.osz)
+    ft⁻¹ = complex_op ? plan_ifft(ft_work1) : plan_irfft(ft_work1, padded_len)
+    
+    frequencies = complex_op ? fftfreq(padded_len) : rfftfreq(padded_len)
+    ωₛ = points[1] * 2π * frequencies / δ # Phases due to the original domain offset
+    
+    # NOTE: This is normalized (δ and scale)
+    conv_shift = exp.(-im * ωₛ) * δ * ft⁻¹.scale
+    xcorr_shift = conj.(conv_shift)
+    fft_x⁻¹ = -im * sign.(frequencies) * ft⁻¹.scale
+    
+    new{T}(weights,ft,ft⁻¹,frequencies,padded,ft_work1,ft_work2,ft⁻¹_work,
+      conv_shift,xcorr_shift,fft_x⁻¹)
   end
-  #
-  LinearGrid(::Type{T}, args...; kwargs...) where {T<:Real} = LinearGrid(args...; kwargs..., T=T)
 end
 
 """
 Convolution of 2 functions defined on a linear grid
 """
-function convolution(lingrid::LinearGrid, u::AbstractVector, v::AbstractVector)
-  @unpack padded, f1, f2, out, FT⁻¹, FT, conv_shift = lingrid.op
-
-  unsafe_execute!(FT, _zeropad!(padded, u), f1)
-  unsafe_execute!(FT, _zeropad!(padded, v), f2)
+function convolution(grid::LinearGrid, u::AbstractVector, v::AbstractVector)
+  (; padded, ft_work1, ft_work2, ft⁻¹_work, ft, ft⁻¹, conv_shift) = grid.op
+  
+  unsafe_execute!(ft, unsafe_copyto!((padded, u), f1FFT)
+  unsafe_execute!(ft, _zeropad!(padded, v), f2)
   @. f2 *= f1 * conv_shift
   unsafe_execute!(FT⁻¹.p, f2, out)
   return out[1:lingrid.L]
@@ -76,8 +107,8 @@ end
 Cross-Correlation of 2 functions defined on a linear grid
 """
 function crosscorrelation(lingrid::LinearGrid, u::AbstractVector, v::AbstractVector)
-  @unpack padded, f1, f2, out, FT⁻¹, FT, corr_shift = lingrid.op
-
+  (; padded, f1, f2, out, FT⁻¹, FT, corr_shift) = lingrid.op
+  
   unsafe_execute!(FT, _zeropad!(padded, u), f1)
   unsafe_execute!(FT, _zeropad!(padded, v), f2)
   @. f2 *= conj(f1) * corr_shift
@@ -89,8 +120,8 @@ end
 Hilbert transformation of a function defined on a linear grid
 """
 @inline function hilbert(lingrid::LinearGrid, u::AbstractVector)
-  @unpack padded, f1, out, FT⁻¹, FT, fft_1_x = lingrid.op
-
+  (; padded, f1, out, FT⁻¹, FT, fft_1_x) = lingrid.op
+  
   unsafe_execute!(FT, _zeropad!(padded, u), f1)
   @. f1 *= fft_1_x
   unsafe_execute!(FT⁻¹.p, f1, out)
